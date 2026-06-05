9f733e4f62adea1c2f4529a987b8894bd2e23e61
adoretes <adoretes@163.com>
2026-06-05 12:45:52 +0800

    优化滚动按钮

--- lib/widgets/scroll_float_button.dart
+++ lib/widgets/scroll_float_button.dart
@@ -16,22 +16,41 @@ class ScrollFloatButton extends StatefulWidget {
   State<ScrollFloatButton> createState() => _ScrollFloatButtonState();
 }
 
-class _ScrollFloatButtonState extends State<ScrollFloatButton> {
-  bool _isVisible = false;
+class _ScrollFloatButtonState extends State<ScrollFloatButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<double> _scaleAnimation;
+  late final Animation<double> _opacityAnimation;
   Timer? _hideTimer;
-  bool _showTop = false;
-  bool _showBottom = false;
+  bool _atTop = false;
+  bool _atBottom = false;
 
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+    _scaleAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutBack,
+      reverseCurve: Curves.easeInCubic,
+    );
+    _opacityAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutCubic,
+      reverseCurve: Curves.easeInCubic,
+    );
     widget.scrollController.addListener(_onScroll);
+    WidgetsBinding.instance.addPostFrameCallback((_) => _onScroll());
   }
 
   @override
   void dispose() {
     widget.scrollController.removeListener(_onScroll);
     _hideTimer?.cancel();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -40,44 +59,27 @@ class _ScrollFloatButtonState extends State<ScrollFloatButton> {
     final maxExtent = pos.maxScrollExtent;
     final minExtent = pos.minScrollExtent;
     final offset = pos.pixels;
-    final canScrollFurther = maxExtent - minExtent > 0;
+    final canScroll = maxExtent - minExtent > 0;
 
-    bool showTop;
-    bool showBottom;
-    if (widget.isReversed) {
-      showTop = canScrollFurther && offset < maxExtent - 1;
-      showBottom = canScrollFurther && offset > minExtent + 1;
-    } else {
-      showTop = canScrollFurther && offset > minExtent + 1;
-      showBottom = canScrollFurther && offset < maxExtent - 1;
-    }
-
-    final changed = showTop != _showTop || showBottom != _showBottom;
-
-    if (changed) {
-      setState(() {
-        _showTop = showTop;
-        _showBottom = showBottom;
-      });
-    }
-
-    if (changed && (showTop || showBottom)) {
-      setState(() {
-        _isVisible = true;
-      });
-      _resetHideTimer();
-    }
-  }
-
-  void _resetHideTimer() {
-    _hideTimer?.cancel();
-    _hideTimer = Timer(const Duration(seconds: 3), () {
-      if (mounted) {
-        setState(() {
-          _isVisible = false;
-        });
+    setState(() {
+      if (widget.isReversed) {
+        _atTop = offset >= maxExtent - 1;
+        _atBottom = offset <= minExtent + 1;
+      } else {
+        _atTop = offset <= minExtent + 1;
+        _atBottom = offset >= maxExtent - 1;
       }
     });
+
+    _hideTimer?.cancel();
+    if (canScroll) {
+      _animationController.forward();
+      _hideTimer = Timer(const Duration(seconds: 3), () {
+        if (mounted) _animationController.reverse();
+      });
+    } else {
+      _animationController.reverse();
+    }
   }
 
   void _scrollToVisualTop() {
@@ -106,28 +108,34 @@ class _ScrollFloatButtonState extends State<ScrollFloatButton> {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return AnimatedOpacity(
-      opacity: _isVisible ? 1.0 : 0.0,
-      duration: const Duration(milliseconds: 200),
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Opacity(
+          opacity: _opacityAnimation.value,
+          child: Transform.scale(
+            scale: _scaleAnimation.value,
+            alignment: Alignment.bottomRight,
+            child: child,
+          ),
+        );
+      },
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          if (_showTop)
-            _buildButton(
-              icon: Icons.arrow_upward,
-              tooltip: '到顶',
-              onTap: _scrollToVisualTop,
-              colorScheme: colorScheme,
-            ),
-          if (_showBottom) ...[
-            const SizedBox(height: 8),
-            _buildButton(
-              icon: Icons.arrow_downward,
-              tooltip: '到底',
-              onTap: _scrollToVisualBottom,
-              colorScheme: colorScheme,
-            ),
-          ],
+          _buildButton(
+            icon: Icons.arrow_upward,
+            tooltip: '到顶',
+            onTap: _atTop ? null : _scrollToVisualTop,
+            colorScheme: colorScheme,
+          ),
+          const SizedBox(height: 8),
+          _buildButton(
+            icon: Icons.arrow_downward,
+            tooltip: '到底',
+            onTap: _atBottom ? null : _scrollToVisualBottom,
+            colorScheme: colorScheme,
+          ),
         ],
       ),
     );
@@ -136,22 +144,26 @@ class _ScrollFloatButtonState extends State<ScrollFloatButton> {
   Widget _buildButton({
     required IconData icon,
     required String tooltip,
-    required VoidCallback onTap,
+    required VoidCallback? onTap,
     required ColorScheme colorScheme,
   }) {
-    return Material(
-      elevation: 4,
-      borderRadius: BorderRadius.circular(24),
-      color: colorScheme.surface.withValues(alpha: 0.55),
-      surfaceTintColor: colorScheme.surfaceTint,
-      child: InkWell(
+    final enabled = onTap != null;
+    return Opacity(
+      opacity: enabled ? 1.0 : 0.3,
+      child: Material(
+        elevation: 4,
         borderRadius: BorderRadius.circular(24),
-        onTap: onTap,
-        child: Container(
-          width: 30,
-          height: 30,
-          alignment: Alignment.center,
-          child: Icon(icon, size: 15, color: colorScheme.onSurface),
+        color: colorScheme.surface.withValues(alpha: 0.55),
+        surfaceTintColor: colorScheme.surfaceTint,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(24),
+          onTap: onTap,
+          child: Container(
+            width: 30,
+            height: 30,
+            alignment: Alignment.center,
+            child: Icon(icon, size: 15, color: colorScheme.onSurface),
+          ),
         ),
       ),
     );
